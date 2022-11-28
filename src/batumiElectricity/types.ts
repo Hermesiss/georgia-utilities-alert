9dea10ab89af8bb5f732f957ec7f496ee6c69b5f
Hermesis <hermesiss@gmail.com>
2022-11-28 13:03:21 +0400

    Format affected areas

--- src/batumiElectricity/types.ts
+++ src/batumiElectricity/types.ts
@@ -10,6 +10,27 @@ export enum PlanType {
   Unplanned = 2 | 3
 }
 
+export class AreaTree {
+  name: string;
+  children = new Map<string, AreaTree>()
+
+  public get(name: string) {
+    return this.children.get(name)
+  }
+
+  public set(name: string, area: AreaTree) {
+    this.children.set(name, area)
+  }
+
+  constructor(name: string = "") {
+    this.name = name;
+  }
+
+  has(name: string) {
+    return this.children.has(name)
+  }
+}
+
 export class Alert {
   taskId: number;
   taskName: string;
@@ -26,6 +47,8 @@ export class Alert {
   startDate: Date;
   endDate: Date;
   planType: PlanType
+
+  areaTree: AreaTree = new AreaTree("Root")
 
   static from(from: Alert): Alert {
     const result = Object.assign(new Alert(), from)
@@ -45,7 +68,7 @@ export class Alert {
     }
   }
 
-  public getPlanText(){
+  public getPlanText() {
     switch (this.planType) {
       case PlanType.Planned:
         return "Planned"
@@ -67,15 +90,31 @@ export class Alert {
     const taskName = await Translator.getTranslation(this.taskName)
     const taskCaption = await Translator.getTranslation(this.scName)
     const regionName = await Translator.getTranslation(this.regionName)
-    const disconnectionArea = await Translator.getTranslation(this.disconnectionArea)
     const planText = this.planType != PlanType.Planned ? ` _${this.getPlanText()}_ ` : ""
+    const areas = await this.formatAreas(this.areaTree);
 
     return `${this.getPlanEmoji()}${planText} *[${taskCaption}]* ${taskName}\n\n` +
       `*Start:* ${this.disconnectionDate}\n\n` +
       `*End:* ${this.reconnectionDate}\n\n` +
       `*Region:* ${regionName}\n\n` +
-      `*Area:* ${disconnectionArea}\n\n` + //TODO date
+      `*Area:*\n${areas}\n\n` + //TODO date
       taskNote
+  }
+
+  public async formatAreas(areaTree: AreaTree, level = 0): Promise<string> {
+    let text = ""
+    if (level > 5) return text
+
+    if (level != 0) {
+      const translatedName = await Translator.getTranslation(areaTree.name)
+      text += `${"    ".repeat(level - 1)}${translatedName}\n`;
+    }
+    for (let [name, area] of areaTree.children) {
+      const text1 = await this.formatAreas(area, level + 1);
+      text += text1
+    }
+
+    return text
   }
 
   private init(): void {
@@ -83,6 +122,24 @@ export class Alert {
     this.endDate = new Date(this.reconnectionDate)
 
     this.planType = this.taskType == "1" ? PlanType.Planned : PlanType.Unplanned
+
+    const areas = this.disconnectionArea.split(',')
+
+    for (let area of areas) {
+      const sub = area.split("/")
+      let tree = this.areaTree
+      for (let i = 0; i < sub.length; i++) {
+        const item = sub[i].trim()
+        const existingTree = tree.get(item)
+        if (!existingTree) {
+          const childTree = new AreaTree(item)
+          tree.set(item, childTree)
+          tree = childTree
+        } else {
+          tree = existingTree
+        }
+      }
+    }
   }
 }
 
