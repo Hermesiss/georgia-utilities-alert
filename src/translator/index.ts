7d0c7a2eda69bcdf74f961465472c3f3d7ca76b2
Hermesis <hermesiss@gmail.com>
2022-12-05 10:48:42 +0400

    Fix translation error with too many requests Translations

--- src/translator/index.ts
+++ src/translator/index.ts
@@ -7,8 +7,16 @@ export class Translator {
     let result = this.translations.get(phrase)
 
     if (!result) {
-      result = await translate(phrase, "ka", "en", false)
-      this.translations.set(phrase, result)
+      console.log("Translating", phrase)
+      try {
+        result = await translate(phrase, "ka", "en", false)
+        await new Promise(r => setTimeout(r, 50)) //wait to avoid ECONNRESET
+        this.translations.set(phrase, result)
+      }
+      catch (e) {
+        console.error(`Translation error while translating [${phrase}]`, e)
+        return phrase
+      }
     }
 
     return result?.translation || phrase
